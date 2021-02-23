6cc0d0584ce949900ce98c079efee0cc1666a9cb
Ghaidaa-Alomran <ghaidaa.aziz18@gmail.com>
2021-02-23 09:07:15 +0300

    adding comment

--- app/src/main/java/home/application/assignment2application/MainActivity.java
+++ app/src/main/java/home/application/assignment2application/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     EditText e;
     TextView t;
 
-
+//hello
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
